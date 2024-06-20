ef97345599b839f5a955521a9fd9536e27955627
JoeyFlaum <58762590+Therealaveragejoe@users.noreply.github.com>
2024-06-20 16:03:02 -0500

    save

--- main.py
+++ main.py
@@ -18,21 +18,32 @@ class Apple:
         self.parent_screen.blit(self.image, (self.x, self.y))
         pygame.display.flip()
 
+    def move(self):
+        self.x = SIZE * random.randint(0, 24)
+        self.y = SIZE * random.randint(0, 19)
+
 
 class Snake:
     def __init__(self, parent_screen, length):
         self.parent_screen = parent_screen
         self.block = pygame.image.load("resources/block.jpg").convert()
+        self.length = length
         self.x = [SIZE] * length
         self.y = [SIZE] * length
-        self.length = length
+
         self.direction = random.choice(possible_directions)  # random starting direction
+
+    def grow(self):
+        self.length += 1
+        self.x.append(2)
+        self.y.append(2)
 
     def draw(self):
         self.parent_screen.fill((110, 110, 5))
         for i in range(self.length):
             self.parent_screen.blit(self.block, (self.x[i], self.y[i]))
-#           pygame.display.flip()
+
+    #           pygame.display.flip()
 
     def move_up(self):
         self.direction = 'up'
@@ -68,9 +79,9 @@ class Snake:
 class Game:
     def __init__(self):
         pygame.init()
-        self.surface = pygame.display.set_mode((1000, 1000))
+        self.surface = pygame.display.set_mode((1000, 800))
         self.surface.fill((110, 110, 5))
-        self.snake = Snake(self.surface, 10)
+        self.snake = Snake(self.surface, 1)
         self.snake.draw()
         self.apple = Apple(self.surface)
         self.apple.draw()
@@ -81,16 +92,22 @@ class Game:
                 return True
         return False
 
-    def play(self, collision_count):
+    def play(self):
         self.snake.walk()
         self.apple.draw()
+        self.display_score()
+        pygame.display.flip()
         if self.is_collision(self.snake.x[0], self.snake.y[0], self.apple.x, self.apple.y):
-            collision_count[0] += 1
-            print(f"collision {collision_count},snake x1: {self.snake.x[0]}, snake y1: {self.snake.y[0]}, apple x2:{self.apple.x}, apple y2:{self.apple.y}" )
+            self.apple.move()
+            self.snake.grow()
 
+
+    def display_score(self):
+        font = pygame.font.SysFont('arial', 30)
+        score = font. render(f"Score: {self.snake.length}",True,(255,255,255))
+        self.surface.blit(score,(800,10))
     def run(self):
         running = True
-        collision_count = [0]
         while running:
             for event in pygame.event.get():
                 if event.type == KEYDOWN:
@@ -106,8 +123,8 @@ class Game:
                         self.snake.move_right()
                 elif event.type == QUIT:
                     running = False
-            time.sleep(.3)  # snake speed
-            self.play(collision_count)
+            time.sleep(.2)  # snake speed
+            self.play()
 
 
 if __name__ == "__main__":
